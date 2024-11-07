84cc8c8e406bfcab36f7086a7a81cfe845053c08
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-11-07 20:09:53 +0100

    fix-bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vladyslavJs.github.io', // мій сайт після деплою
-    base: 'framework-Astro-',
+    base: '/framework-Astro-',
     output: 'static',
 });
